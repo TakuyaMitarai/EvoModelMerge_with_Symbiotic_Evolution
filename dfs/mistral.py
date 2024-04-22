4ddee3bcbeac4d9893b27979bf63b2f89176a0bc
TakuyaMitarai <miterphoto@gmail.com>
2024-04-23 00:17:16 +0900

    mistralも追加

--- dfs/mistral.py
+++ dfs/mistral.py
@@ -1,8 +1,9 @@
 import torch
 import torch.nn as nn
 import numpy as np
-from transformers import AutoTokenizer, T5Tokenizer, AutoConfig
-from transformers.models.auto.modeling_auto import AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import transformers.models.mistral.modeling_mistral
+from MistralDecoderLayer import SCALE_MistralDecoderLayer
 import random
 
 def set_seed(seed_value):
@@ -11,7 +12,7 @@ def set_seed(seed_value):
     torch.manual_seed(seed_value)
     torch.cuda.manual_seed_all(seed_value)
 
-# scaling
+#scaling
 scale_factors = [1.1, 0.8, 1.05, 0.9] * 10
 new_scale_factors = scale_factors
 
@@ -20,28 +21,34 @@ random.shuffle(layer_indices)
 for new_idx, original_idx in enumerate(layer_indices):
     new_scale_factors[new_idx] = scale_factors[original_idx]
 
-tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-v0.1")
-model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-v0.1",
-    torch_dtype=torch.float16,
+class SCALE_MistralDecoderLayer2(SCALE_MistralDecoderLayer):
+    def __init__(self, config, layer_idx):
+        super().__init__(config, layer_idx)
+        self.scaling_factors = new_scale_factors
+
+# transformers.models.mistral.modeling_mistral.MistralDecoderLayer = SCALE_MistralDecoderLayer2
+
+tokenizer = AutoTokenizer.from_pretrained("SakanaAI/EvoLLM-JP-v1-7B")
+model = AutoModelForCausalLM.from_pretrained("SakanaAI/EvoLLM-JP-v1-7B",
+    torch_dtype=torch.float32,
     load_in_4bit=True,
     device_map="auto",
     trust_remote_code=False,)
-# tokenizer = T5Tokenizer.from_pretrained("rinna/japanese-gpt2-medium")
-# custom_model = AutoModelForCausalLM.from_pretrained("rinna/japanese-gpt2-medium")
-# tokenizer = AutoTokenizer.from_pretrained("openai-community/gpt2")
-# original_model = AutoModelForCausalLM.from_pretrained("openai-community/gpt2")
-# custom_model = original_model
+custom_model = AutoModelForCausalLM.from_pretrained("SakanaAI/EvoLLM-JP-v1-7B",
+    torch_dtype=torch.float32,
+    load_in_4bit=True,
+    device_map="auto",
+    trust_remote_code=False,)
 
 # arrangement
-# layer_indices = list(range(len(original_model.transformer.h)))
-# random.shuffle(layer_indices)
-# for new_idx, original_idx in enumerate(layer_indices):
-#     custom_model.transformer.h[new_idx] = original_model.transformer.h[original_idx]
-
+layer_indices = list(range(len(model.model.layers)))
+random.shuffle(layer_indices)
+for new_idx, original_idx in enumerate(layer_indices):
+    custom_model.model.layers[new_idx] = model.model.layers[original_idx]
+    custom_model.model.layers[new_idx].self_attn.layer_idx = new_idx
 # set_seed(46)
-input_ids = tokenizer.encode("りんごは何色ですか？", return_tensors="pt")
-
-outputs = custom_model.generate(input_ids, do_sample=True, max_length=50, num_return_sequences=3)
-
+input_ids = tokenizer.encode("ローブを作成するには、青色の繊維を2巻分、白色の繊維をその半分用いる必要があります。全体で何巻必要ですか？", return_tensors="pt")
+input_ids = input_ids.to('cuda')
+outputs = custom_model.generate(input_ids, do_sample=True, max_length=200, num_return_sequences=2)
 decoded_outputs = tokenizer.batch_decode(outputs, skip_special_tokens=True)
 print(decoded_outputs)
